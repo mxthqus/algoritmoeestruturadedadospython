3d54d4e6216210caab0131873466456467206e45
Matheus Magalhães <82778376+mxthqus@users.noreply.github.com>
2024-10-30 17:57:56 -0400

    Add files via upload

---
+++ PythonVSCode/30out/print5.py
@@ -0,0 +1,11 @@
+if 5 > 2:
+    print("5 is bigger than 2!")
+
+    else:
+        print("5 is not bigger than 2!")
+    
+    for i in range(5):
+        i = i + x 
+        y = 3
+        x = y+1
+        print(i)
